886e0087b7ac07f67dee63c49a0dc3fced6a2ca1
Teddy Leno <lenoteddy@gmail.com>
2025-07-28 20:08:53 +0700

    feat: 🔧 add logo, title & dashboard structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { useState } from "react";
 import { ConnectKitButton } from "connectkit";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Logo from "./assets/logo.webp";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div>
-			<div className="flex justify-center items-center">
-				<ConnectKitButton />
-			</div>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		<div className="container">
+			<header className="py-6 flex justify-center items-center">
+				<div className="flex items-center justify-between">
+					<img src={Logo} alt="Logo" className="w-12 h-12 rounded-full" />
+					<h1 className="ml-2 text-2xl font-semibold">PaperMint</h1>
+				</div>
+				<div className="ml-auto ">
+					<ConnectKitButton />
+				</div>
+			</header>
+			<main>
+				<div></div>
+			</main>
+			<footer>
+				<div></div>
+			</footer>
 		</div>
 	);
 }

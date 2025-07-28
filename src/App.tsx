e651f0eeff1dda7c3b32d1893f38a7407edccabd
Teddy Leno <lenoteddy@gmail.com>
2025-07-28 17:13:28 +0700

    feat: 🎨 setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { ConnectKitButton } from "connectkit";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { Web3Provider } from "./Web3Provider";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
